2348d799abac75ec107641695e6c114c6137b3ee
Yash Varshney <yashvarshneypepsi@gmail.com>
2022-04-10 12:38:36 +0530

    In this commit I learn how to works on images or assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main (){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar:AppBar(title:Text("yash varshney"),backgroundColor: Colors.amber, ),
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text("Dicee"),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        body: DicePage(),
       ),
+    );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Image.asset('images/dice2.png')
+        ),
+        Expanded(
+          child: Image.asset('images/dice2.png')
+          ),
+        
+      ],
     );
   }
 }
